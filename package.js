f6f78e95c7ffba58d0800fc8ec3b333609891f80
Mitar <mitar.git@tnode.com>
2014-09-14 01:34:18 -0700

    Transitory package.

--- package.js
+++ package.js
@@ -1,26 +1,10 @@
 Package.describe({
   summary: "Simple XML to JavaScript object converter",
   version: '0.4.4-2',
-  name: 'peerlibrary:xml2js',
+  name: 'mrt:xml2js',
   git: 'https://github.com/peerlibrary/meteor-xml2js.git'
 });
 
-Npm.depends({
-  xml2js: '0.4.4'
-});
-
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1.1');
-  api.use('peerlibrary:blocking@0.4.2');
-
-  api.export('xml2js');
-
-  api.add_files([
-    'server.js'
-  ], 'server');
-});
-
-Package.on_test(function (api) {
-  api.use(['peerlibrary:xml2js', 'tinytest', 'test-helpers'], ['server']);
-  api.add_files('tests.js', ['server']);
+  api.imply('peerlibrary:xml2js@0.4.4-2');
 });
--- server.js
+++
@@ -1,3 +0,0 @@
-xml2js = Npm.require('xml2js');
-
-xml2js.parseStringSync = blocking(xml2js.parseString);
