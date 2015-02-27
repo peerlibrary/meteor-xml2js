6a2b829559b9d511e95be693882d419f08069b6a
Mitar <mitar.git@tnode.com>
2015-02-26 22:00:01 -0800

    Version bump.

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   summary: "Simple XML to JavaScript object converter",
-  version: '0.4.4_4',
+  version: '0.4.5_1',
   name: 'peerlibrary:xml2js',
   git: 'https://github.com/peerlibrary/meteor-xml2js.git'
 });
 
 Npm.depends({
-  xml2js: '0.4.4'
+  xml2js: '0.4.5'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@1.0.2.1');
-  api.use('peerlibrary:blocking@0.5.1');
+  api.versionsFrom('METEOR@1.0.3.2');
+  api.use('peerlibrary:blocking@0.5.2');
 
   api.export('xml2js');
 
